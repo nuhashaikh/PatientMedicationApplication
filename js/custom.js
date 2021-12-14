e07c4acaca0a15e29566fecec4f75e47bfcae95c
Nuha <nuha.shaikh@gmail.com>
2021-12-14 10:24:14 -0700

    UI for chat

--- js/custom.js
+++ js/custom.js
@@ -104,7 +104,17 @@
             return false;
         });
     });
+    (function ($) {
+    $("#live-chat header").on("click", function () {
+        $(".chat").slideToggle(300, "swing");
+        $(".chat-message-counter").fadeToggle(300, "swing");
+    });
 
+    $(".chat-close").on("click", function (e) {
+        e.preventDefault();
+        $("#live-chat").fadeOut(300);
+    });
+    })(jQuery);
     /* ==============================================
      CODE WRAPPER -->
      =============================================== */
